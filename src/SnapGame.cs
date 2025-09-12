f8a431c6ce2feb9e9c360bae7cfebd2e7a56442b
Rabeya Islam Rima <151783031+Rabeya-55@users.noreply.github.com>
2025-09-12 10:20:56 +0600

    Update SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -18,15 +18,14 @@ namespace CardGames
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
 		private static void HandleUserInput(Snap myGame)
-		{
-			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
-
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
-				myGame.FlipNextCard ();
-			}
-		}
+        {
+            //Fetch the next batch of UI interaction
+            SwinGame.ProcessEvents();
+            if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
+            {
+				myGame.Start ();
+            }
+        }
 
 		/// <summary>
 		/// Draws the game to the Window.
